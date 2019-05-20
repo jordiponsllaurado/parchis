151fb8254c07f6c80bb7d56a80b5395db87d4fac
jordi.pons <jordi.pons@typeform.com>
2019-05-20 09:16:58 +0200

    Fist parchis board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  renderFinalPath (color) {
+    return (
+      <React.Fragment>
+        <div className={`cell ${color}`}></div>
+        <div className={`cell ${color}`}></div>
+        <div className={`cell ${color}`}></div>
+        <div className={`cell ${color}`}></div>
+        <div className={`cell ${color}`}></div>
+        <div className={`cell ${color}`}></div>
+      </React.Fragment>
+    )
+  }
+
+  renderPath (start, end, color) {
+    let itemList = []
+    for (let i = start; i <= end; ++i) {
+      const circle = (i === start + 7) || (i === start + 12)? 'circle' : ''
+      const col =  (i === start + 12) ? color : ''
+      const number = i%68 === 0 ? 68 : i%68
+      itemList.push(<div className={`cell p${i - start + 1} ${circle} ${col}`}>{number}</div>)
+    }
+    return itemList
+  }
+
+  render () {
+    return (
+      <div className="Board">
+        <div className='box red circle'></div>
+        <div className='box blue circle'></div>
+        <div className='box green circle'></div>
+        <div className='box yellow circle'></div>
+        <div className='box play down'>
+          { this.renderFinalPath('yellow') }
+          { this.renderPath(61, 75, 'yellow')}
+        </div>
+        <div className='box play right'>
+          { this.renderFinalPath('blue') }
+          { this.renderPath(10, 24, 'blue')}
+        </div>
+        <div className='box play top'>
+          { this.renderFinalPath('red') }
+          { this.renderPath(27, 41, 'red')}
+        </div>
+        <div className='box play left'>
+          { this.renderFinalPath('green') }
+          { this.renderPath(44, 58, 'green')}
+        </div>
+        <div className='centerBoard'></div>
+      </div>
+    )
+  }
 }
 
 export default App;
